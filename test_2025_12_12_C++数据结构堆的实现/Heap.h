2921319e734438778549df14149bf4876475ad14
JYY97409 <16382184+jyy97409@user.noreply.gitee.com>
2025-12-12 23:08:45 +0800

    2025/12/12/C++堆（不完善）

---
+++ test_2025_12_12_C++数据结构堆的实现/Heap.h
@@ -0,0 +1,109 @@
+#pragma once
+#include<iostream>
+typedef int HPDataType;
+class HP
+{
+private:
+	HPDataType* _hp;
+	int _size;//此处的size指向的是下一个元素
+	int _capacity;
+public:
+	HP(HPDataType arr[], int capacity = 4)
+	{
+		if (nullptr != arr)
+		{
+			_hp = (HPDataType*)malloc(sizeof(HPDataType)*(sizeof(arr)/sizeof(HPDataType)));
+			_capacity = (sizeof(arr) / sizeof(HPDataType));
+			_size = _capacity - 1;
+		}
+		else
+		{
+			_hp = (HPDataType*)malloc(sizeof(HPDataType) * capacity);
+			_size = 0;
+			_capacity = capacity;
+		}
+	}
+	~HP()
+	{
+		free(_hp);
+	}
+	void Swap(HPDataType* p1, HPDataType* p2)
+	{
+		HPDataType temp = 0;
+		temp = *p1;
+		*p1 = *p2;
+		*p2 = temp;
+	}
+	void HpPush(HPDataType x)
+	{
+		if (_size == _capacity)//需要扩容
+		{
+			HPDataType* parr = (HPDataType*)realloc(_hp, _capacity + 4 * sizeof(HPDataType) );
+			if (nullptr == parr)
+			{
+				perror("realloc");
+				return;
+			}
+			_hp = parr;
+		}
+		_hp[_size] = x;
+		_size++;
+		int child = _size - 1;
+		int parent = 0;
+		while (child > 0)
+		{
+			parent = (child - 1) / 2;
+			if (_hp[parent] > _hp[child])
+			{
+				Swap(&_hp[parent], &_hp[child]);
+				child = parent;
+			}
+			else
+			{
+				break;
+			}
+		}
+	}
+	void HeapifyDown()//向下调整
+	{
+		int parent = 0;
+		int child = 0;
+		HPDataType* small = nullptr;
+		while (parent * 2 + 1 < _size)//儿子不能超出范围
+		{
+			child = parent * 2 + 1;
+			small = &_hp[child];
+			if (_hp[child] > _hp[child + 1])
+			{
+				small = &_hp[child + 1];
+			}
+			if (*small < _hp[parent])
+			{
+				Swap(&_hp[parent], small);
+			}
+			parent = child;
+		}
+	}
+
+	HPDataType HpPop()
+	{
+		if (0 == _size)
+		{
+			std::cout << "堆中没有数据" << std::endl;
+		}
+		else if (1 == _size)
+		{
+			return _hp[_size--];
+		}
+		else
+		{
+			HPDataType ret = _hp[_size - 1];
+			Swap(&_hp[0], &_hp[_size - 1]);
+			_size--;//相当于是将最后一个元素排除出去了
+			HeapifyDown();
+			return ret;
+		}
+
+	}
+
+};
---
+++ test_2025_12_12_C++数据结构堆的实现/test.cpp
@@ -0,0 +1,13 @@
+#include"Heap.h"
+int main()
+{
+	class HP hp1(nullptr);
+	hp1.HpPush(1);
+	hp1.HpPush(2);
+	hp1.HpPush(3);
+	hp1.HpPush(4);
+	hp1.HpPush(2);
+	std::cout << hp1.HpPop() << std::endl;
+
+	return 0;
+}
